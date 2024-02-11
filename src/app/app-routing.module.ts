022ea23e9775cb3b99ae4a4a679265134d2e18c5
Vijay <vijay@neverskip.com>
2024-02-11 08:51:00 +0530

    routing check

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'gymnastics',
+    loadChildren: () => import('./gymnastics/gymnastics.module').then(m => m.GymnasticsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
