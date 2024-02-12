d092377b5e21519b6c4475def67d0002f6a0ec2a
Vijay <vijay@neverskip.com>
2024-02-12 10:49:58 +0530

    Check

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './gymnastics/list/list.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+  },
   {
     path: 'gymnastics',
     loadChildren: () => import('./gymnastics/gymnastics.module').then(m => m.GymnasticsModule)
